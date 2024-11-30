4c6873dea02eeb34e2a60dd4ff1c20656d752970
jhwptsd <jhwptsd@gmail.com>
2024-11-29 19:32:48 -0500

    Altered parse_json.py and consolidated train_protify.ipynb

--- parse_json.py
+++ parse_json.py
@@ -1,7 +1,8 @@
 import json
 import sys
 
-def parse_json(path, max_len=150):
+def parse_json(path, a, b, max_len=150):
+    num = -1
     seqs = {}
     comps = []
     macros = []
@@ -10,10 +11,14 @@ def parse_json(path, max_len=150):
     for i in data["train_set"]:
         for j in data["train_set"][i]:
             for k in data["train_set"][i][j]:
+                num = num + 1
                 if data["train_set"][i][j][k]["length"]>max_len:
                     continue
-                seqs[k]=data["train_set"][i][j][k]["sequence"]
-                comps.append(i)
-                macros.append(j)
+                if num>=a and num<=b:
+                    seqs[k]=data["train_set"][i][j][k]["sequence"]
+                    comps.append(i)
+                    macros.append(j)
+                if num>b:
+                    break
     f.close()
     return seqs, comps, macros
