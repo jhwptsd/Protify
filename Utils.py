Fixed gradient flow

Utils.py
@@ -100,8 +100,7 @@ def parse_rna(path):
             y = float(y)
             z = float(z)
 
-            point = np.add(np.array([x,y,z]), correction_factor)
-
+            point = torch.tensor([x, y, z], dtype=torch.float32, requires_grad=True) + correction_factor
             if atom == "P":
               if (correction_factor==torch.zeros(3)).all():
                 correction_factor = torch.tensor([-x, -y, -z])
@@ -113,7 +112,7 @@ def parse_rna(path):
                 angle_points.append(point)
                 v1 = angle_points[-1]-angle_points[-2]
                 v2 = angle_points[-3]-angle_points[-2]
-                norms.append(np.cross(v1, v2))
+                norms.append(torch.cross(v1, v2))
                 angle_points = []
 
         return torch.tensor(points, requires_grad=True, dtype=torch.float32), torch.tensor(norms, requires_grad=True, dtype=torch.float32)
@@ -146,7 +145,7 @@ def parse_protein(path):
             y = float(y)
             z = float(z)
 
-            point = np.add(np.array([x,y,z]), correction_factor)
+            point = torch.tensor([x, y, z], dtype=torch.float32, requires_grad=True) + correction_factor
             if atom == "CA":
               if (correction_factor==torch.zeros(3)).all():
                 correction_factor = torch.tensor([-x, -y, -z])
@@ -158,7 +157,7 @@ def parse_protein(path):
                 angle_points.append(point)
                 v1 = angle_points[-1]-angle_points[-2]
                 v2 = angle_points[-3]-angle_points[-2]
-                norms.append(np.cross(v1, v2))
+                norms.append(torch.cross(v1, v2))
                 angle_points = []
 
         return torch.tensor(points, requires_grad=True), torch.tensor(norms, requires_grad=True)
@@ -197,7 +196,7 @@ def protein_to_rna(protein, rna_path, corrector, tm=False):
 def correct_protein_coords(points, corrector):
     vectors = points[1:] - points[:-1]
     norms = torch.norm(vectors, dim=1, keepdim=True)
-    normalized_vectors = vectors / norms
+    normalized_vectors = vectors / (norms+1e-8)
     corrected_vectors = normalized_vectors * corrector
     corrected_points = torch.cat([points[:1], points[:-1] + corrected_vectors], dim=0)
     return corrected_points

train_protify_local.py
@@ -214,7 +214,7 @@ def train(seqs, epochs=50, batch_size=32,tm_score=False, max_seq_len=150, conver
     conv.train()
 
     # Set up optimizers
-    optimizer = torch.optim.AdamW(conv.parameters(), lr=0.01)
+    optimizer = torch.optim.AdamW(conv.parameters(), lr=0.001)
 
     dataloader = torch.utils.data.DataLoader(seqs, batch_size=batch_size, shuffle=True, drop_last=True, num_workers=8, pin_memory=True)
 
@@ -225,9 +225,9 @@ def train(seqs, epochs=50, batch_size=32,tm_score=False, max_seq_len=150, conver
     
     # Training loop
     for epoch in range(epochs):
+        torch.cuda.empty_cache()
+        torch.cuda.synchronize()
         for seqs, tags in dataloader:
-            torch.cuda.empty_cache()
-            torch.cuda.synchronize()
             optimizer.zero_grad(set_to_none=True)
 
             with torch.autocast(device_type="cuda"):
@@ -235,7 +235,7 @@ def train(seqs, epochs=50, batch_size=32,tm_score=False, max_seq_len=150, conver
 
                 # LAYER 1: RNA-AMINO CONVERSION
                 processed_seqs = [torch.tensor(np.transpose(encode_rna(s), (0, 1)), dtype=torch.float32) for s in seqs] # (batch, seq, base)
-                lengths = [len(s) for s in processed_seqs]
+                lengths = sum([len(s) for s in processed_seqs])
 
                 # Pad sequences
                 for i in range(len(processed_seqs)):
@@ -253,20 +253,18 @@ def train(seqs, epochs=50, batch_size=32,tm_score=False, max_seq_len=150, conver
                 write_fastas(final_seqs)
 
                 # LAYER 2: PROTEIN FOLDING
-                lengths, loss = loss_fn(final_seqs)
+                loss = loss_fn(final_seqs)
 
             lengths = torch.tensor(lengths / batch_size, dtype=torch.float32)
             losses.append(loss)
 
             empty_dir("FASTAs", delete=False)
-            loss = torch.mean(loss)
             print(f"\n\nCurrent Loss: {loss}")
             print(f"Average Loss per Residue: {loss/lengths}")
             loss_pr.append(loss/lengths)
-            loss.requires_grad = True
             loss.backward()
             
-            nn.utils.clip_grad_norm_(c.parameters(), 1.0)
+            nn.utils.clip_grad_norm_(conv.parameters(), 1.0)
             
             optimizer.step()
             if tm_score:
@@ -297,7 +295,6 @@ class ProteinFoldLoss(nn.Module):
   
    def forward(self, final_seqs):
       num_gpus = torch.cuda.device_count()
-      lengths = sum(len(seq) for seq in final_seqs.values())
 
       jobnames = list(final_seqs.keys())
       fasta_files = [f'FASTAs/{name}.fasta' for name in jobnames]
@@ -312,14 +309,12 @@ class ProteinFoldLoss(nn.Module):
       for jobname, _ in results:
           empty_dir(jobname)
 
-      return lengths, loss.mean()
+      return loss.mean()
         
 
 
 def run_parallel(fasta_file, jobname, gpu_id):
     os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu_id)
-
-    torch.cuda.set_device(0)
 
     queries, _ = get_queries(fasta_file)
     