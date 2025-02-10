Fixed mean P-P dist calculation

Utils.py
@@ -99,11 +99,14 @@ def tm_score(p1, p2, lt):
     distance = torch.norm(p1 - p2, dim=-1)
     return -torch.mean(1 / (1 + (distance / d0).pow(2)))
 
-def parse_rna(path):
+def parse_rna(path, return_skips=False):
     parser = MMCIFParser(QUIET=True)
     structure = parser.get_structure("RNA", path)
     data = []
     nucleotides = {'A', 'U', 'C', 'G'}
+
+    skips = []
+    count = 0
 
     for model in structure:
         for chain in model:
@@ -112,6 +115,9 @@ def parse_rna(path):
                     for atom in residue:
                         vector = atom.get_vector()
                         data.append((vector[0], vector[1], vector[2], atom.get_name().strip()))
+                else:
+                    skips.append(count)
+                    count += 1
 
     points = []
     angle_points = []
@@ -137,7 +143,12 @@ def parse_rna(path):
                 norms.append(torch.cross(v1, v2))
 
                 angle_points = [] 
-
+    if return_skips:
+        return (
+            torch.stack(points, dim=0) if points else torch.empty(0, 3, requires_grad=True),
+            torch.stack(norms, dim=0) if norms else torch.empty(0, 3, requires_grad=True),
+            skips
+        )
     return (
         torch.stack(points, dim=0) if points else torch.empty(0, 3, requires_grad=True),
         torch.stack(norms, dim=0) if norms else torch.empty(0, 3, requires_grad=True)

phosphodiester_length.py
@@ -29,13 +29,9 @@ def get_structure(tag, path):
         return path
 
 def mean(arr):
-     sum = 0
-     total = 0
-     for i in arr:
-          for j in i:
-            total += 1
-            sum += j
-     return sum/total
+    total = sum(sum(i) for i in arr)
+    count = sum(len(i) for i in arr)
+    return total / count if count else 0 
 
 if __name__=="__main__":
 
@@ -61,11 +57,12 @@ if __name__=="__main__":
     
     # Parse sequences and get the average phosphodiester bond length
     for i in tqdm(range(len(old_seqs))):
-         pts = parse_rna(get_structure(list(old_seqs.keys())[i], struct_path))[0].detach().cpu().numpy()
+         pts, _, skips = parse_rna(get_structure(list(old_seqs.keys())[i], struct_path))
+         pts = pts.detach().cpu().numpy()
          temp_lengths = []
          for i in range(len(pts)-1):
+              if i in skips:
+                   continue
               temp_lengths.append(np.linalg.norm(pts[i]-pts[i+1]))
-        #  temp_lengths = np.mean(np.array(temp_lengths))
          lengths.append(temp_lengths)
-    #lengths = np.array(lengths)
     print(mean(lengths))