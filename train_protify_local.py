11032a78f500663c341a85251939925678d8c828
jhwptsd <jhwptsd@gmail.com>
2025-02-11 20:09:39 -0500

    Update train_protify_local.py

--- train_protify_local.py
+++ train_protify_local.py
@@ -258,10 +258,10 @@ def train(seqs, epochs=50, batch_size=32,tm_score=False, max_seq_len=150, conver
 
         with open("losses.txt", "w") as txt_file:
           for line in losses:
-              txt_file.write(" ".join(line) + "\n")
+              txt_file.write(f"{line}\n")
         with open("losses_pr.txt", "w") as txt_file:
           for line in loss_pr:
-              txt_file.write(" ".join(line) + "\n")
+              txt_file.write(f"{line}\n")
         torch.save(conv, f'ConverterWeights/converter_epoch_{epoch}.pt')
         torch.save(conv.state_dict(), f'ConverterWeights/converter_params_epoch_{epoch}.pt')
 
